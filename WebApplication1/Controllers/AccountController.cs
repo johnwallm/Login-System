81feae412ff9824d94a572c032b0af35d1400098
johnwallm <64194445+johnwallm@users.noreply.github.com>
2020-08-01 15:32:03 +0800

    Initial commit

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Registered model)
+        {
+            using (var context = new CapstoneDemoEntities())
+            {
+                bool isValid = context.User.Any(x => x.Username == model.Username && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return RedirectToAction("Index", "User Information");
+                }
+
+                ModelState.AddModelError("", "Invalid Username and Password");
+
+                return View();
+            }
+        }
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(UserInformation model)
+        {
+            using (var context = new CapstoneDemoEntities())
+            {
+                context.UserInformation.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
